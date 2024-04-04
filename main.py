1d2226fa5b5d15b1de8bea9b10f39c36ae108486
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-04-04 09:08:59 +0000

    get first link iteration 1

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 
+def isInteresting(link):
+    # print(link)
+    if(("role" in link.parent.attrs) and ("note" in link.parent.attrs["role"])):
+        return False
+    if("class" in link.attrs):
+        return False
+    if(link.parent.name == "figcaption"):
+        return False
+    if(link.parent.name != "p"):
+        return False
+    return True
+
+
 r = requests.get('https://en.wikipedia.org/wiki/Special:Random')
-# r = requests.get('https://en.wikipedia.org/wiki/Sultan_Haydar')
 soup = BeautifulSoup(r.content, 'html.parser')
 
 artTitle = soup.title.text
@@ -14,7 +26,7 @@ contentLinks = soup.body.find("div", id="mw-content-text").find_all("a")
 links = []
 
 for link in contentLinks:
-    if("class" in link.attrs):
-        continue
-    links.append(link)
+    if(isInteresting(link)):
+        links.append(link)
+
 print(links[0])
