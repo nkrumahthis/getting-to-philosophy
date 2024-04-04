5e34707536c112637bef30a763b44ad1921e10d2
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-04-04 03:30:41 +0000

    get first link

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 I read/heard somewhere years ago that if you click on the first link of any Wikipedia article long enough, you get to either the article on Mathematics or Philosophy.
 
 I wrote this scraper to test that.
+
+## Usage
+
+```bash
+python main.py
+```
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import requests
 
 r = requests.get('https://en.wikipedia.org/wiki/Special:Random')
+# r = requests.get('https://en.wikipedia.org/wiki/Sultan_Haydar')
+soup = BeautifulSoup(r.content, 'html.parser')
 
-print(r.content)
+artTitle = soup.title.text
+
+print(artTitle)
+
+# get first link
+contentLinks = soup.body.find("div", id="mw-content-text").find_all("a")
+links = []
+
+for link in contentLinks:
+    if("class" in link.attrs):
+        continue
+    links.append(link)
+print(links[0])
