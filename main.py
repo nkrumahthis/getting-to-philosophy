c75b2d5160438cf140d283448f5d1dd9e3400962
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-04-05 23:04:55 +0000

    get first link of the p

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ import requests
 
 def isInteresting(link):
     # print(link)
+    # print(link.parent.name)
     if(("role" in link.parent.attrs) and ("note" in link.parent.attrs["role"])):
         return False
     if("class" in link.attrs):
         return False
-    if(link.parent.name == "figcaption"):
-        return False
-    if(link.parent.name != "p"):
-        return False
+    # if(link.parent.name == "figcaption"):
+    #     return False
+    # if(link.parent.name != "p" and link.parent.name != "b"):
+    #     return False
     return True
 
 
 r = requests.get('https://en.wikipedia.org/wiki/Special:Random')
+# r = requests.get('https://en.wikipedia.org/wiki/Lutz_Sch%C3%BClbe')
+
 soup = BeautifulSoup(r.content, 'html.parser')
 
 artTitle = soup.title.text
@@ -22,11 +25,33 @@ artTitle = soup.title.text
 print(artTitle)
 
 # get first link
-contentLinks = soup.body.find("div", id="mw-content-text").find_all("a")
-links = []
+# contentLinks = soup.body.find("div", id="mw-content-text").find_all("a")
+# links = []
 
-for link in contentLinks:
-    if(isInteresting(link)):
-        links.append(link)
+allp = soup.body.find("div", id="mw-content-text").find_all("p")
 
-print(links[0])
+# print(allp)
+
+thep = []
+
+for p in allp:
+    if("class" in p.attrs and "mw-empty-elt" in p.attrs["class"]):
+        continue
+    thep.append(p)
+
+print(thep[0].a)
+
+# contentLinks = soup.body.find("div", id="mw-content-text").p.find_all("p")
+
+# print(soup.body.find("div", id="mw-content-text"))
+# links = []
+
+# for link in contentLinks:
+#     # print(link)
+#     if(isInteresting(link)):
+#         links.append(link)
+
+# if(len(links) == 0):
+#     print("No links found")
+# else:
+#     print(links[0])
