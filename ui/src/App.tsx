d3b82b2622f942af52feaa5c70bee19a7475520e
Nkrumah Sarpong <nkrumahthis@gmail.com>
2024-05-20 00:07:54 +0000

    stream crawl events to UI

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,16 +1,40 @@
-// import { useState } from 'react'
 
 import './App.css'
 
-const handleClick = () => {
-  console.log('clicked')
+const url = 'http://localhost:5002';
+interface Hit {
+  previous: string,
+  current: string
+}
+
+const handleCrawl = () => {
+  const eventSource = new EventSource(`${url}/stream`);
+
+    eventSource.onmessage = (event) => {
+      const newMessage:Hit = JSON.parse(event.data);
+      console.log(newMessage);
+      // setMessages((prevMessages) => [...prevMessages, newMessage]);
+    };
+
+    eventSource.onerror = (error) => {
+      console.error('EventSource failed:', error);
+      eventSource.close();
+    };
+
 }
 
 function App() {
 
+  // const [nodes, setNodes] = useState<Hit[]>([])
+
   return (
     <div className=''>
-      <button onClick={handleClick}>Crawl</button>
+      <h1 className="text-xl">
+        Getting to Philosophy!
+      </h1>
+      
+      <button onClick={handleCrawl} className='px-4 py-1 border bg-blue-700 text-white rounded-full'>Crawl </button>
+
     </div>
   )
 }
